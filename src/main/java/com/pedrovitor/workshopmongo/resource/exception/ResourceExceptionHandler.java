4dab17013e82d6bc9cc79ad90f85033491d4f3ce
Pedro Vitor <pedrocoud12@gmail.com>
2024-02-24 09:56:44 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/pedrovitor/workshopmongo/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.pedrovitor.workshopmongo.resource.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.pedrovitor.workshopmongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNoyFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(
+				System.currentTimeMillis(),
+				status.value(),
+				"Não encontrado",
+				e.getMessage(),
+				request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}
